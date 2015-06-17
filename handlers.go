bf0fbdc75b82663b3e3864b8af49c3b02e8ebe3b
Luke Slattery <lukeslattery@Lukes-MacBook-Pro.local>
2015-06-17 19:43:13 +0100

    Adding 'Content-Type' & 'Status'

--- handlers.go
+++ handlers.go
@@ -7,9 +7,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
+
 func Index( w, http.ResponseWriter, r *http.Request ) {
 	fmt.Fprintln( w, "Welcome!")
 }
+
 
 func TodoIndex( w http.ResponseWriter, r *http.Request ) {
 	todo := Todos {
@@ -17,10 +19,14 @@ func TodoIndex( w http.ResponseWriter, r *http.Request ) {
 		Todo{ Name: "Host Meeting" },
 	}
 
+	w.Header().Set( "Content-Type", "application/json; charset=UTF-8" )
+	w.WriteHeader( http.StatusOK )
+
 	if err := json.NewEncoder( w ).Encode( todos ); err != nil {
 		panic(err)
 	}
 }
+
 
 func TodoShow( w http.ResponseWriter, r *http.Request ) {
 	vars := mux.Vars( r )
