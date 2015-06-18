34f479795305b0997cb889a71db4b6aad4c392b0
Luke Slattery <lukeslattery@Lukes-MacBook-Pro.local>
2015-06-18 09:41:28 +0100

    Adding Test Instructions

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Run this curl command to test if we can post some data
+# Go to: http://localhost/todos to see the response from the API
 
 curl -H "Content-Type: applictaion/json" -d '{ "name": "Now Todo" }' http://localhost:8080/todos
-
